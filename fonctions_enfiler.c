79f6acbfdb6cec8ca65ee44ff126f63cb60877b2
Rebai Chedly <107418294+ChedlyRebai@users.noreply.github.com>
2022-06-14 17:33:54 +0200

    ..

--- fonctions_enfiler.c
+++ fonctions_enfiler.c
@@ -1,1 +1,17 @@
-#include<stdio.h>
+
+#include<stdlib.h> //pour utiliser la fonctions malloc
+
+file empfiler(file f,int e){
+    pfile p;
+    p=(file)malloc(sizeof(cell));
+    p->info=e;
+    p->suiv=NULL;
+    if(f->t==NULL){
+        f->t=p;
+        f->q=p;
+    }else{
+        f->q->suiv=p;
+        f->q=p;
+    }
+}
+ 
