d2ad53524fbbbccd0b691243b6034f3859c9fc59
Rebai Chedly <107418294+ChedlyRebai@users.noreply.github.com>
2022-06-14 17:46:58 +0200

    C

--- Emfiler.c
+++ Emfiler.c
@@ -1,17 +1,23 @@
 
 #include<stdlib.h> //pour utiliser la fonctions malloc
 
-file empfiler(file f,int e){
-    pfile p;
-    p=(file)malloc(sizeof(cell));
-    p->info=e;
-    p->suiv=NULL;
-    if(f->t==NULL){
-        f->t=p;
-        f->q=p;
-    }else{
-        f->q->suiv=p;
-        f->q=p;
+file emfiler(file f,int e)
+{
+    file ft,p;  
+    ft=(file)malloc(sizeof(cellule));
+    ft->info=e;
+    ft->suivant=NULL;
+    if(f==NULL)
+    {
+        f=ft;
+    }else
+    {
+    p=f;
+    while(p->suivant!=NULL){
+        p=p->suivant;
     }
+    p->suivant=ft;
+    }
+    return f;
 }
  
